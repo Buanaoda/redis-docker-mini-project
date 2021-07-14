5bb3277fd8f4535e97ebf9afaf9ab641e50f98fa
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-07-14 16:49:29 -0300

    added redis connection config

--- cache/redis.js
+++ cache/redis.js
@@ -1,6 +1,5 @@
 const redis = require('promise-redis')();
-const client = redis.createClient();
-// exports.client = redis.createClient({ host: 'redis', port: 6379});
+exports.client = redis.createClient({ host: 'redis', port: 6379});
 
 exports.saveToCacheUsingKeyValueWith = async (key, value) => {
   await client.set(key, JSON.stringify(value), 'EX', 15);
